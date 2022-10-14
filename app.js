f1c33ad759765d88628378fb96bcbd75ba496d39
Smuuse2 <Smuuse09@gmail.com>
2022-10-14 21:10:00 +0300

    Model JavaScript

---
+++ app.js
@@ -0,0 +1,12 @@
+
+        let popup = document.getElementById("saed");
+        function  openPopup (){
+popup.classList.add("open-popup")
+
+        }
+
+
+        function  closePopup (){
+popup.classList.toggle("open-popup")
+
+        }
